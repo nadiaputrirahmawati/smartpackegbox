4dbdad10ffbbdf0782d9a158b180889d769970a4
Irez Abdullah <irezabdullah18@gmail.com>
2025-12-25 15:29:33 +0700

    setup interia-vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Laravel 12 + Inertia + Vue</h1>
+    <p>Project MQTT Gateway Service Laravel</p>
+  </div>
+</template>
